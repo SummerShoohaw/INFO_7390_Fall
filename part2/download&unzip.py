d74a43a6f56194a38494471ee6936f9a07640de8
Shuhao Xia <xia.shu@husky.neu.edu>
2018-10-10 13:28:31 -0400

    save

--- part2/download&unzip.py
+++ part2/download&unzip.py
@@ -6,9 +6,11 @@ import zipfile
 import os
 from bs4 import BeautifulSoup
 
+
+yr = input("Please input the YEAR：")
+year = str(yr)
 # download .zip file from website
-def downloading(year):
-    year = str(year)
+def downloading():
     url_total = 'https://www.sec.gov/files/EDGAR_LogFileData_thru_Jun2017.html'
 
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -16,30 +18,26 @@ def downloading(year):
 
     res_total = r'www.sec.gov/dera/data/Public-EDGAR-log-file-data/'+year+'/Qtr[0-9]/log[0-9]+01\.zip'
     lst = re.findall(res_total,response.read().decode('utf-8'))
-
     for i in range(len(lst)):
         url = 'https://'+lst[i]
         month = str(i+1)
         filename = year+"_"+month+"_01.zip"
-        response = rq.urlopen(url,context = ctt)
         rq.urlretrieve(url,filename)
 
 # un-zip files
-def un_zip(year,month):
-    year = str(year)
-    month = str(month)
-    file_name = year+'_'+month+'_01.zip'
-    folder_path = year + '_' + month + "_01"
-    zip_file = zipfile.ZipFile(file_name)
-    if os.path.isdir(folder_path):
-        pass
-    else:
-        os.mkdir(folder_path)
-    for names in zip_file.namelist():
-        zip_file.extract(names,folder_path + '/')
-    zip_file.close()
+def un_zip():
+    for i in range(12):
+        month = str(i+1)
+        file_name = year+'_'+month+'_01.zip'
+        folder_path = 'extract_'+year
+        zip_file = zipfile.ZipFile(file_name)
+        if os.path.isdir(folder_path):
+            pass
+        else:
+            os.mkdir(folder_path)
+        for names in zip_file.namelist():
+            zip_file.extract(names,folder_path + '/')
+        zip_file.close()
 
-#TEST: download all files in year 2016
-downloading(2016)
-#TEST: un zip file '2016_1_01.zip'(just downloaded) to folder '2016_1_01'
-un_zip(2016,1)
+downloading()
+un_zip()
