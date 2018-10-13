591e40677e494fc54f4e4f7754242e3830bd925b
chiwenshi <shi.ch@husky.neu.edu>
2018-10-12 22:57:33 -0400

    <a>

--- dockerDemo/test.py
+++ dockerDemo/test.py
@@ -1,13 +1,2 @@
-import pandas
-import csv
-
-df = pandas.DataFrame([1,2,3],[4,5,6])
-import os
-path = "./test"
-if os.path.isdir(path):
-    pass
-else: os.mkdir(path)
-df.to_csv("./test/test.csv", index = False, header = True)
-
-df1 = pandas.read_csv("./test/test.csv")
-print(df1)
+a = input('Please input:')
+print(a)
