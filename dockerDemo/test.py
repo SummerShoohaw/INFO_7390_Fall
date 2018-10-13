a783cc3a3001bc4e8ba6be9862edfc27e411d573
chiwenshi <shi.ch@husky.neu.edu>
2018-10-12 20:40:10 -0400

    <test docker>

--- dockerDemo/test.py
+++ dockerDemo/test.py
@@ -1,7 +1,13 @@
-print("hello world1")
-print("hello world2")
-print("hello world3")
-print("hello world4")
-print("hello world5")
-print("hello world6")
+import pandas
+import csv
 
+df = pandas.DataFrame([1,2,3],[4,5,6])
+import os
+path = "./test"
+if os.path.isdir(path):
+    pass
+else: os.mkdir(path)
+df.to_csv("./test/test.csv", index = False, header = True)
+
+df1 = pandas.read_csv("./test/test.csv")
+print(df1)
